b74d995580c56a7006a53a0ba5c13440b124f6d1
IMURANI975 <imuranimicah@gmail.com>
2024-08-05 19:00:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var me=document.getElementById("me");
+var hidden=document.getElementById("hidden");
+
+function Window(){
+    document.getElementById("hidden").style.display="block";
+}
+function main(){
+    document.getElementById("hidden").style.display="none";
+}
